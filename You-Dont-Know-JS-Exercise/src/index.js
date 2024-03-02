8a4be546c5ed827708cb3da21d865711418d6d5b
yangheat <yangheat@gamil.com>
2024-03-02 23:55:58 +0900

    tailwind 초기 설정

--- You-Dont-Know-JS-Exercise/src/index.js
+++ You-Dont-Know-JS-Exercise/src/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import CompareExercise from './compare-exercise/compare-exercise'
+import './index.css'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello, World!</div>,
+    element: <h1 class="text-3xl font-bold underline">Hello world!</h1>,
   },
   {
     path: '/compare-exercise',
