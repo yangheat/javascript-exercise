269406ffbcb6936aa8d52ce1eaf1458117cfca1d
SunPark <yangheat@gmail.com>
2024-03-12 12:02:21 +0900

    react-router-dom 추가로 틱택톡 경로 변경

--- react-dev-learn/src/index.js
+++ react-dev-learn/src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import TicTacToc from './tic-tac-toc/TicTacToc';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Hello World!</h1>
+  },
+  {
+    path: '/tic-tac-toc',
+    element: <TicTacToc />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
