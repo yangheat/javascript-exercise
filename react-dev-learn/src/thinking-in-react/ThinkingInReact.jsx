1027174f188175fe4ac38fab7f842608cbd88d70
SunPark <yangheat@gmail.com>
2024-03-18 12:27:33 +0900

    예제 구현

--- react-dev-learn/src/thinking-in-react/ThinkingInReact.jsx
+++ react-dev-learn/src/thinking-in-react/ThinkingInReact.jsx
@@ -1,25 +1,5 @@
+import { useState } from 'react';
 import './ThinkingInReact.css'
-
-const PRODUCTS = [
-  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
-  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
-  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
-];
-
-function SearchBar() {
-  return (
-    <form>
-      <input type="text" />
-      <label>
-        <input type="checkbox" />
-          Only show products in stock
-      </label>
-    </form>
-  )
-}
 
 function ProductCategoryRow({ category }) {
   return (
@@ -46,12 +26,21 @@ function ProductRow({ product }) {
 }
 
 
-function ProductTable() {
+function ProductTable({ products, filterText, inStockOnly }) {
   const rows = []
   let lastCategory = null
-  PRODUCTS.forEach((product) => {
+
+  products.forEach((product) => {
+    if (!product.name.toLowerCase().includes(filterText.toLowerCase())) {
+      return
+    }
+
     if (product.category !== lastCategory) {
       rows.push(<ProductCategoryRow category={product.category} key={product.category} />)
+    }
+
+    if (inStockOnly && product.stocked === false) {
+      return
     }
 
     rows.push(<ProductRow product={product} key={product.name} />)
@@ -72,11 +61,36 @@ function ProductTable() {
   )
 }
 
+function SearchBar({ filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange }) {
+  return (
+    <form>
+      <input type="text" placeholder="Search..." value={filterText} onChange={(e) => onFilterTextChange(e.target.value)}/>
+      <label>
+        <input type="checkbox" checked={inStockOnly} onChange={(e) => onInStockOnlyChange(e.target.value)} />
+          {' '}
+          Only show products in stock
+      </label>
+    </form>
+  )
+}
+
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
 export default function ThinkingInReact () {
+  const [filterText, setFilterText] = useState('')
+  const [inStockOnly, setInStockOnly] = useState(false)
+
   return (
     <>
-      <SearchBar />
-      <ProductTable />
+      <SearchBar filterText={filterText} inStockOnly={inStockOnly} onFilterTextChange={setFilterText} onInStockOnlyChange={setInStockOnly} />
+      <ProductTable products={PRODUCTS} filterText={filterText} inStockOnly={inStockOnly} />
     </>
   )
 }
